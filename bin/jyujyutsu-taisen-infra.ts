04e4206b10c95407cfd31b53263700fcdef02b6e
Tomohiro Yoshida <rollwithit0117@gmail.com>
2020-12-04 23:30:29 +0900

    COGNITOスタック削除

--- bin/jyujyutsu-taisen-infra.ts
+++ bin/jyujyutsu-taisen-infra.ts
@@ -2,13 +2,11 @@
 import 'source-map-support/register';
 import config from 'config';
 import * as cdk from '@aws-cdk/core';
-import { CognitoStack } from '../lib/cognito/cognito-stack';
 import { StepFunctionsStack } from '../lib/step-functions/step-functions-stack';
 
 const app = new cdk.App();
 (async() => {
     const systemNameUpperCase = config.get<string>('systemName').toUpperCase();
-    const cognitoStack = new CognitoStack(app, `${systemNameUpperCase}-COGNITE-STACK`)
     const stepFunctionsStack = new StepFunctionsStack(app, `${systemNameUpperCase}-STEP-FUNCTIONS-STACK`)
 })().catch(e => {
     console.error(e);
